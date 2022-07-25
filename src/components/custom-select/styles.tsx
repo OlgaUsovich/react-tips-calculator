795b51901d3a1e508060af98712941f28ad05b33
Olga <olga@gmail.com>
2022-07-25 22:06:09 +0300

    refactor(hw-38): refactor select styles

--- src/components/custom-select/styles.tsx
+++ src/components/custom-select/styles.tsx
@@ -1,32 +1,33 @@
-export const customStyles = {
-    control: (styles: any, state: any) => ({ // ??? типы
-      ...styles,
-      borderRadius: '30px',
-      height: '68px',
-      textAlign: 'center',
-      color: '#756C6C',
-      border: state.isFocused ? '2px solid rgba(117, 108, 108, 0.57)' : 0,
-      boxShadow: state.isFocused ? '2px solid rgba(117, 108, 108, 0.57)' : 0,
-    '&:hover': {
-       border: state.isFocused ? '2px solid rgba(117, 108, 108, 0.57)' : 0
-    }
-      
-    }),
+import { ControlProps, CSSObjectWithLabel, GroupBase, StylesConfig } from "react-select";
 
-    input: (styles: any) => ({ // ??? типы
-        ...styles,
-        color: '#756C6C',
-        fontSize: '18px',
-      }),
-    indicatorSeparator: () => ({
-        display: 'none',
-      }),
-    indicatorContainer: (styles: any) => ({ // ??? типы
-        ...styles,
-        color: '#756C6C',
-        width: '22px',
-        heigth: '10px',
-      }),
-    
-    
-}
+export const customStyles: StylesConfig = {
+  control: (styles: CSSObjectWithLabel, state: ControlProps<unknown, boolean, GroupBase<unknown>>) => ({
+    ...styles,
+    borderRadius: "30px",
+    height: "68px",
+    textAlign: "center",
+    color: "#756C6C",
+    border: state.isFocused ? "2px solid rgba(117, 108, 108, 0.57)" : 0,
+
+    "&:hover": {
+      border: state.isFocused ? "2px solid rgba(117, 108, 108, 0.57)" : 0,
+    },
+  }),
+
+  input: (styles: CSSObjectWithLabel) => ({
+    ...styles,
+    color: "#756C6C",
+    fontSize: "18px",
+  }),
+
+  indicatorSeparator: () => ({
+    display: "none",
+  }),
+
+  dropdownIndicator: (styles: CSSObjectWithLabel) => ({
+    ...styles,
+    color: "#756C6C",
+    // width: '22px',
+    // heigth: '10px',
+  }),
+};
